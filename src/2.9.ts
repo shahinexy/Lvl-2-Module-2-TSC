6aef57a5ccd97dd9099efb214afb0d9c9a179b96
shahinexy <shahinexy@gmail.com>
2024-11-09 22:12:17 +0600

    Conditional

---
+++ src/2.9.ts
@@ -0,0 +1,28 @@
+{
+    // Conditional
+    type a1 = null;
+
+    type b1 = undefined;
+
+    type x = a1 extends null ? true : false;
+
+    type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+
+
+    type Sheikh = {
+        bike: string;
+        car: string;
+        ship : string;
+        plane : string
+    }
+
+    type CheckVehicle<T> = T extends keyof Sheikh ? true : false;
+
+    type HaseShip = CheckVehicle<"ship"> // true
+
+    type HaseTracktor = CheckVehicle<"Tracktor"> // false
+
+
+    //
+}
